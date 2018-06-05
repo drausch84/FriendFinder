17c322fbf1240f561cff727029aeddbf35ee211d
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-06-04 21:45:09 -0600

    created friends.js file and styled home and survey html

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+module.exports = [
+{
+    name:"Eeyore",
+    photo:"https://pbs.twimg.com/profile_images/480490796521492480/bZUrXTxi.png",
+    scores:["1","1","1","1","1","1","1","1","1","1"]
+},
+{
+    name:"Ben Stein",
+    photo:"https://hightimes.com/wp-content/uploads/2016/04/ben-stein.jpg",
+    scpres:["2","2","2","2","2","2","2","2","2","2"]
+},
+{
+    name:"Zach Braff",
+    photo:"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2NjExOTA1MDM1/the-clinton-foundations-a-decade-of-difference-gala.jpg",
+    scpres:["3","3","3","3","3","3","3","3","3","3"]
+},
+{
+    name:"Katy Perry",
+    photo:"http://geekvox.com.br/wp-content/uploads/2014/03/Katy-Perry-Last-Friday-Night-TGIF-music-video.jpg",
+    scpres:["4","4","4","4","4","4","4","4","4","4"]
+},
+{
+    name:"Tom Cruise",
+    photo:"https://i.ytimg.com/vi/pVu5qH7gw2A/hqdefault.jpg",
+    scpres:["5","5","5","5","5","5","5","5","5","5"]
+},
+{
+    name:"Jon Snow",
+    photo:"http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/06/250116GOTJonSnow_SYK_250116-1-920x610.jpg",
+    scpres:["2","3","1","4","2","3","1","5","2","2"]
+},
+{
+    name:"Sheila Hammond",
+    photo:"https://s3.r29static.com//bin/entry/1b8/0,0,2000,2400/720x864/1740542/image.png",
+    scpres:["4","3","3","4","5","1","2","5","4","4"]
+},
+{
+    name:"Jesse Pinkman",
+    photo:"https://cdn.shopify.com/s/files/1/1877/8853/files/jesse_pinkman_article_image_3_grande.jpg?v=1501934608",
+    scpres:["2","2","5","1","1","5","5","4","3","2"]
+},
+{
+    name:"Fox Mulder",
+    photo:"https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-x-files-david-duchovny.jpg",
+    scpres:["2","2","3","2","4","1","3","3","2","4"]
+},
+{
+    name:"Dale Cooper",
+    photo:"https://3ojv801zx6ub12ssdy326u7b-wpengine.netdna-ssl.com/wp-content/uploads/2017/05/dale-cooper-1-750x400.jpg",
+    scpres:["4","4","4","4","4","4","5","5","5","5"]
+},
+{
+    name:"Barney The Dinosaur",
+    photo:"https://pbs.twimg.com/profile_images/1447299295/barney2_400x400.png",
+    scpres:["5","5","5","5","5","5","5","5","2","2"]
+},
+{
+    name:"Dennis Reynolds",
+    photo:"https://d3p157427w54jq.cloudfront.net/uploads/2017/03/DennisReynolds-619-386.jpg",
+    scpres:["1","1","1","1","2","2","2","5","1","1"]
+},
+{
+    name:"Rick Sanchez",
+    photo:"https://pbs.twimg.com/profile_images/870105716702445570/NGZ7ZMjw_400x400.jpg",
+    scpres:["1","5","1","5","1","5","1","5","1","5"]
+}
+];
