04d256fff44fd3630d05c08f63dc2965a5d4f616
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-06-05 01:09:49 -0600

    my routes aren't being found

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-// Pull in required dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-// Configure the Express application
 var app = express();
+
 var PORT = process.env.PORT || 8080;
 
-// Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
 
-// Add middleware for parsing incoming request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Add the application routes
-require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-// Start listening on PORT
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+	console.log("App listening on PORT: " + PORT);
 });
